2d9fa3f15f7e993474d9cea47c10c87c1e23c987
emrbalc1 <emrbalc160@gmail.com>
2023-06-28 15:42:00 +0300

    Yolcu Sayma uygulaması yapıldı

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # passengercounterv2
+
+-- Bu bir tren yolcu sayısı uygulaması
+-- Bu uygulama Yıldız Teknik Üniversite Sürekli Eğitim Merkezi öğrencilerine ödev olarak verilmiştir.
+-- Uygulamanın orjinal hali buradaki linkte bulunmaktadır.
---
+++ script.js
@@ -0,0 +1,42 @@
+let saveEl = document.getElementById("save-el")
+let gecmisBilgi = localStorage.getItem("gecmisBilgi")
+if( gecmisBilgi !== null) {
+    saveEl.textContent = localStorage.getItem("gecmisBilgi")
+} else {
+    saveEl.textContent = ""
+}
+
+
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    localStorage.setItem( "gecmisBilgi", saveEl.textContent )
+    countEl.textContent = 0
+    count = 0
+}
+
+let totalEl = document.querySelector("#total-people")
+
+function showTotal() {
+  let gecmisBilgi = localStorage.getItem("gecmisBilgi")
+  let gecmisSayilar = gecmisBilgi.split("-")
+  let toplam = 0
+
+  for (let i = 0; i < gecmisSayilar.length; i++) {
+    toplam += Number(gecmisSayilar[i].trim())
+  }
+
+  totalEl.textContent = "Total People: " + toplam
+}
+
+showTotal()
+
+
